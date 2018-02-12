21fed28e10fe9ecf6f54419c873d19c1fea6f5db
Maurice <maurice.bijron@hotmail.com>
2018-02-12 15:15:22 +0100

    edited setup script

--- with_installer@1.1.2/setup.py
+++ with_installer@1.1.2/setup.py
@@ -1,4 +1,3 @@
-from PyWorks.IO.File import File
-from PyWorks.IO.Path import Path
+from pyworks.io import file, path
 
 File.write_text(Path.combine(script_path, "generated.txt"), "This is a bunch of text created by the setup.py script")
