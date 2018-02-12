c3faf1f56e33c997a72d10a3cad85b1b03d6c5b9
Maurice <maurice.bijron@hotmail.com>
2018-02-12 13:51:36 +0100

    added use of local variables in installer

--- with_installer@1.1.2/setup.py
+++ with_installer@1.1.2/setup.py
@@ -1,4 +1,4 @@
 from PyWorks.IO.File import File
 from PyWorks.IO.Path import Path
 
-File.write_text(Path.combine(Path.get_script_path(), "generated.txt"), "This is a bunch of text created by the setup.py script")
+File.write_text(Path.combine(script_path, "generated.txt"), "This is a bunch of text created by the setup.py script")
